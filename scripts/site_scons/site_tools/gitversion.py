025eb3c4f7797b668a86e1a5976f0074c5876f36
Lee Killough <15950023+leekillough@users.noreply.github.com>
2026-07-12 13:54:53 -0500

    Add volatile and reference GIT_COMMIT_STRING to prevent link-time optimization

--- scripts/site_scons/site_tools/gitversion.py
+++ scripts/site_scons/site_tools/gitversion.py
@@ -70,13 +70,14 @@ __amd_libm_version_template="""/*
 * DO NOT EDIT!
 */
 
-static const char VERSION_STRING[] = "%s";
-static const char GIT_COMMIT_STRING[] = "git:%s";
+static const volatile char VERSION_STRING[] = "%s";
+static const volatile char GIT_COMMIT_STRING[] = "git:%s";
 
 static const char* alm_get_build(void);
 
 static const char* alm_get_build(void)
 {
+        (void)GIT_COMMIT_STRING;
         return VERSION_STRING;
 }
 """
